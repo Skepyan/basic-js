3410edfc315cfa319430ad7f500cedfe09ca2ca2
skepyan <skepyan1@yandex.byskepyan1@yandex.by>
2021-02-08 20:50:46 +0300

    count cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function countCats(array) {
+  const result = array.reduce((acc, x) => {
+    if (Array.isArray(x)){
+      acc += countCats(x);
+    } else if (x === '^^'){
+      acc += 1;
+    }
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    return acc;
+    }, 0);
+
+    return result;
 };
+
+
+
+[
+  [0, 1, '^^'],
+  [0, '^^', 2],
+  ['^^', 1, 2]
+]
+
+if (x === '^^'){
+  acc += 1;
+}
