32e6dc5f22bb239e1a16a0cc3770dc697f761ac2
skepyan <skepyan1@yandex.byskepyan1@yandex.by>
2021-02-12 22:34:47 +0300

    feat: add function dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ let teamName = '';
+ if (Array.isArray(members) === false){
+   return false;
+ }else{
+   let emptyArray = [];
+   emptyArray = members.reduce(function(acc, current){
+     if(typeof(current) === 'string'){
+      emptyArray.push(current.trim()[0].toUpperCase());
+      if(current.length !== 0)
+       return emptyArray   ;
+     }
+   },"");
+   if (emptyArray.length > 0)
+   teamName = emptyArray.sort().join('')
+   return teamName;
+   }
+ 
 };
